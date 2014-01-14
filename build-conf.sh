8bd97b36ae5e63a104371f506cb14576e09875f3
Rodrigo Quesada <rodrigoquesada@gmail.com>
2014-01-14 12:22:17 -0600

    Changed configuration for bamboo build directory on build-conf.sh file.

--- build-conf.sh
+++ build-conf.sh
@@ -3,6 +3,6 @@ project_dir_name=crafter-integration
 target_server=saffron.rivetlogic.com
 target_server_user=ci
 
-lr_bamboo_dir=/opt/liferay-bamboo-build-deps
+lr_bamboo_dir=/mnt/bamboo-5.0.1/builds/liferay-bamboo-build-deps
 
 valid_instances=DEV
